2f384e439894ba66964b60cdb02aed48a5464b8c
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-09-18 09:33:06 +0000

    Update dependency webpack to v5.101.3 (#45173)
    * Update dependency webpack to v5.101.3
    
    * Rename the file to fix the build error
    
    * Update test snapshots
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17824008701
    
    Upstream-Ref: Automattic/jetpack@18eb749eec2185dc106f74b36c163ef458dded0f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,15 +10,15 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.7',
+      'ver' => '4.3.8-alpha1758186986',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.8',
+      'ver' => '6.18.9-alpha1758186986',
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
-      'ver' => '0.5.5-alpha1758174413',
+      'ver' => '0.5.5-alpha1758186986',
     ),
   ),
 );
