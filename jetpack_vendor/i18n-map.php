16f706cd5de96df87e5faa1a47b597778a0cc3bf
tbradsha <tbradsha@users.noreply.github.com>
2025-11-06 13:59:37 +0000

    PHP 8.5: Enable CI tests (#45771)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19137603080
    
    Upstream-Ref: Automattic/jetpack@93ad69896a2a1af34f552ffa194c1e52443d44ef

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.12-alpha1762349303',
+      'ver' => '4.3.12-alpha1762436462',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.4-alpha1762349303',
+      'ver' => '6.19.4-alpha1762436462',
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
