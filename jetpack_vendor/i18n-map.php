b33faaa47bea4f07124088c015eb40966d4e6ee2
tbradsha <tbradsha@users.noreply.github.com>
2026-05-13 17:01:53 +0000

    Backport crm 6.8.0 changes (#48778)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25813102139
    
    Upstream-Ref: Automattic/jetpack@2549a500e8de37d3f03ce643e096d01c533e7e4d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.3',
+      'ver' => '0.8.4-alpha1778690619',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.35',
+      'ver' => '4.3.36',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.4',
+      'ver' => '8.3.5-alpha1778690619',
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
