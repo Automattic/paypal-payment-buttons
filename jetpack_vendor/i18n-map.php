c9cf3f2f4485a15a474cf881656d58337f10d167
zaguiini <zaguiini@users.noreply.github.com>
2026-06-16 00:15:08 +0000

    Agents Manager: Release 0.5.0 (#49641)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27584412282
    
    Upstream-Ref: Automattic/jetpack@e90387486c614193053b5c0c975c6143cf340488

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.2',
+      'ver' => '0.9.3',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.43',
+      'ver' => '4.3.44',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.0-alpha1781554849',
+      'ver' => '8.7.0',
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
