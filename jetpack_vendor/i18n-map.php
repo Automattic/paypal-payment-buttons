c0fdba5ad66439224e8f8f6610c70e940eaafe8c
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-06-26 10:16:14 +0000

    Backport social 9.0.3 changes (#50001)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28231008135
    
    Upstream-Ref: Automattic/jetpack@54cafa3faf54651d248617dffbd303c706868873

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.5',
+      'ver' => '0.9.6',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.1',
+      'ver' => '4.4.2',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.5-alpha1782415648',
+      'ver' => '8.7.5',
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
