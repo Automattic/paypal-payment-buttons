1c92cb4c735135df5a2a420c93f017225e6e58d1
anomiex <anomiex@users.noreply.github.com>
2026-07-16 15:28:35 +0000

    Update wordpress monorepo (#50529)
    * Update wordpress monorepo
    * Replace removed `$font-weight-medium` sass var
    * Update jest configs for `@wordpress/theme` going esm-only
      Why did they have to keep the `.mjs` extension?
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29509927840
    
    Upstream-Ref: Automattic/jetpack@161416c54f0b3c735d4035ab1d5675b8f72a5cd9

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.10',
+      'ver' => '0.9.11-alpha1784214611',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.6-alpha1784147716',
+      'ver' => '4.4.6-alpha1784214611',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.9-alpha1784147716',
+      'ver' => '8.7.9-alpha1784214611',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
-      'ver' => '0.7.9-alpha1784159976',
+      'ver' => '0.7.9-alpha1784214611',
     ),
   ),
 );
