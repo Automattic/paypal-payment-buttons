979cb2b84427feca22ca6425079cd7a6ee290f0d
fgiannar <fgiannar@users.noreply.github.com>
2026-09-14 12:22:22 +0000

    Connection: Prevent and unstick stale no_possible_tokens connection errors (#52264)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34841201995
    
    Upstream-Ref: Automattic/jetpack@26adab1f5d999ef19cf403ca4869cc9841d0ee6f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.3.0-alpha1789386091',
+      'ver' => '9.3.0-alpha1789387305',
     ),
     'jetpack-feature-flags' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-feature-flags',
