5fa46ddb234dbf8879787063550ab3c3facdef88
haqadn <haqadn@users.noreply.github.com>
2026-09-22 17:02:48 +0000

    Backport jetpack 16.3-a.3 changes (#52573)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Changelog: apply review suggestions for 16.3-a.3
    
    * Changelog: rewrite and split the entries flagged in review
    
    * Changelog: resort entries after the review edits
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35755949971
    
    Upstream-Ref: Automattic/jetpack@b9836a7a8b9ae8d75ffc3ab8dd32c309d209423a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.13.0-alpha1789762796',
+      'ver' => '0.13.0',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.5.0-alpha1790083762',
+      'ver' => '9.6.0-alpha1790095377',
     ),
     'jetpack-feature-flags' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-feature-flags',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-paypal-payments' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-paypal-payments',
-      'ver' => '0.10.0-alpha1790075714',
+      'ver' => '0.11.0-alpha1790095377',
     ),
   ),
   'paths' => array(
